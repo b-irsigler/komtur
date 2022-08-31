d47ad7eea176a234e2838b73b08d3f79b0369f00
Bernhard Irsigler <bernhard.irsigler@fu-berlin.de>
2022-08-31 21:42:30 +0200

    first steps for character animation

--- game/troll.gd
+++ game/troll.gd
@@ -2,11 +2,20 @@ extends KinematicBody2D
 
 const MOTION_SPEED = 160 # Pixels/second.
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+
 func _physics_process(_delta):
 	var motion = Vector2()
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	motion.y /= float(2)
-	motion = motion.normalized() * MOTION_SPEED
+	
+	if motion != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", motion.normalized())
+		motion = motion.normalized() * MOTION_SPEED
+		print(motion.normalized())
+	if motion == Vector2.ZERO:
+		motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
 	move_and_slide(motion)
