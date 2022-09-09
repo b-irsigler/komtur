171045a784794c61fd3f7ff88f99b0268a172a24
Bernhard Irsigler <bernhard.irsigler@fu-berlin.de>
2022-09-10 00:02:24 +0200

    Add motion

--- game/scenes/christine.gd
+++ game/scenes/christine.gd
@@ -1,12 +1,25 @@
 extends KinematicBody2D
 
-const MOTION_SPEED = 400 # Pixels/second.
+const MOTION_SPEED = 160 # Pixels/second.
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(_delta):
 	var motion = Vector2()
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	motion.y /= float(2)
-	motion = motion.normalized() * MOTION_SPEED
+	
+	if motion != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", motion.normalized())
+		animationTree.set("parameters/Run/blend_position", motion.normalized())
+		animationState.travel("Run")
+		motion = motion.normalized() * MOTION_SPEED
+		print(motion.normalized())
+	if motion == Vector2.ZERO:
+		animationState.travel("Idle")
+		motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
 	move_and_slide(motion)
