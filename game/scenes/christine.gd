17c369c396bc8de654b1a655505183b60cd047ec
d-schn <david.p.schneider@fu-berlin.de>
2022-09-16 22:24:56 +0200

    implemented testing amenities
    christine can now run with shift key
    christine is centered in the map at start
    map ist now square, 200*200 tiles

--- game/scenes/christine.gd
+++ game/scenes/christine.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 const MOTION_SPEED = 160 # Pixels/second.
+const RUN_MULT = 2
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -11,13 +12,15 @@ func _physics_process(_delta):
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	motion.y /= float(2)
-	
+
 	if motion != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", motion.normalized())
 		animationTree.set("parameters/Run/blend_position", motion.normalized())
 		animationState.travel("Run")
-		motion = motion.normalized() * MOTION_SPEED
-		print(motion.normalized())
+		if Input.is_action_pressed("run"):
+			motion = motion.normalized() * MOTION_SPEED * RUN_MULT
+		else:
+			motion = motion.normalized() * MOTION_SPEED
 	if motion == Vector2.ZERO:
 		animationState.travel("Idle")
 		motion = motion.normalized() * MOTION_SPEED
