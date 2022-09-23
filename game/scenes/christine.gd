3826eee57351abc9bea38a24ce91eb7873c95ef1
Bernhard Irsigler <bernhard.irsigler@fu-berlin.de>
2022-09-23 22:38:38 +0200

    implement jump for christine
    needs refactoring!

--- game/scenes/christine.gd
+++ game/scenes/christine.gd
@@ -1,24 +1,37 @@
 extends KinematicBody2D
 
 const MOTION_SPEED = 160 # Pixels/second.
-const RUN_MULT = 2
+const RUN_MULT = 10
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var sprite = $Sprite
+onready var timer  = $Timer
+var jumping = false
+var jump_height = 40
+var jump_duration = 1
 
 func _physics_process(_delta):
 	var motion = Vector2()
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	motion.y /= float(2)
+	
+	if jumping:
+		animationState.travel("stop")
+		sprite.offset = Vector2(0,jump(timer.time_left))
 
 	if motion != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", motion.normalized())
-		animationTree.set("parameters/Run/blend_position", motion.normalized())
-		animationState.travel("Run")
+		if not jumping:
+			animationTree.set("parameters/Idle/blend_position", motion.normalized())
+			animationTree.set("parameters/Run/blend_position", motion.normalized())
+			animationState.travel("Run")
 		if Input.is_action_pressed("run"):
-			motion = motion.normalized() * MOTION_SPEED * RUN_MULT
+			jumping = true
+			timer.start()
+			animationState.travel("stop")
+			motion = motion.normalized() * MOTION_SPEED #* RUN_MULT
 		else:
 			motion = motion.normalized() * MOTION_SPEED
 	if motion == Vector2.ZERO:
@@ -26,3 +39,12 @@ func _physics_process(_delta):
 		motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
 	move_and_slide(motion)
+
+func jump(time):
+	var temp = time / jump_duration
+	return 4 * jump_height * temp * ( temp - 1)
+
+func _on_Timer_timeout():
+	timer.wait_time = jump_duration
+	jumping = false
+	animationState.travel("Run")
