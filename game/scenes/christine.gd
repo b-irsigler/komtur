613adee8e3cfa8e39cb1e9e44594defee1c1bce5
Bernhard Irsigler <bernhard.irsigler@fu-berlin.de>
2022-09-26 21:41:03 +0200

    Refactoring of Christine's state logic

--- game/scenes/christine.gd
+++ game/scenes/christine.gd
@@ -8,47 +8,58 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var sprite = $Sprite
 onready var timer  = $Timer
-var jumping = false
 var running = false
 var jump_height = 40
 var jump_duration = 1
+var motion = Vector2()
+
+enum State {IDLE, WALK, RUN, JUMP}
+var current_state = State.IDLE
 
 func _physics_process(_delta):
-	var motion = Vector2()
+	
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	motion.y /= float(2)
+	motion = motion.normalized() * MOTION_SPEED
 	
-	if jumping:
-		animationState.travel("stop")
-		sprite.offset = Vector2(0,jump(timer.time_left))
+	if motion == Vector2.ZERO:
+		current_state = State.IDLE
+	elif current_state != State.JUMP:
+		current_state = State.WALK
+	
 	if Input.is_action_just_pressed("run"):
 		running = not running
-
-	if motion != Vector2.ZERO:
-		if not jumping:
-			animationTree.set("parameters/Idle/blend_position", motion.normalized())
-			animationTree.set("parameters/Run/blend_position", motion.normalized())
-			animationState.travel("Run")
-		if Input.is_action_pressed("jump"):
-			jumping = true
-			timer.start()
-			animationState.travel("stop")
-		motion = motion.normalized() * MOTION_SPEED
-		if running:
-			motion *= RUN_MULT
+	if Input.is_action_just_pressed("jump") and current_state != State.JUMP:
+		current_state = State.JUMP
+		timer.start()
 		
-	if motion == Vector2.ZERO:
-		animationState.travel("Idle")
-		motion = motion.normalized() * MOTION_SPEED
-	#warning-ignore:return_value_discarded
+	match current_state:
+		State.IDLE:
+			idle()
+		State.WALK:
+			if running:
+				motion *= RUN_MULT
+			walk(motion)
+		State.JUMP:
+			jump()
+			
+func idle():
+	animationState.travel("Idle")
+	
+func walk(motion):
+	animationTree.set("parameters/Idle/blend_position", motion.normalized())
+	animationTree.set("parameters/Run/blend_position", motion.normalized())
+	animationState.travel("Run")
 	move_and_slide(motion)
 
-func jump(time):
-	var temp = time / jump_duration
-	return 4 * jump_height * temp * ( temp - 1)
+func jump():
+	animationState.travel("stop")
+	var temp = timer.time_left / jump_duration
+	sprite.offset = Vector2(0,4 * jump_height * temp * ( temp - 1))
+	move_and_slide(motion)
 
 func _on_Timer_timeout():
 	timer.wait_time = jump_duration
-	jumping = false
 	animationState.travel("Run")
+	current_state = State.IDLE
