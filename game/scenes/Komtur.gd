7daeef6b671092545853d6af5fd876cbf9c5b222
d-schn <david.p.schneider@fu-berlin.de>
2022-11-20 15:31:03 +0100

    Komtur interaction
    Komtur now chases christine in a small radius (same logic as with Spinne) and yells randomly at her

--- game/scenes/Komtur.gd
+++ game/scenes/Komtur.gd
@@ -7,17 +7,36 @@ const max_return_counter = 10
 onready var world = get_parent()
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
-onready var timer = $Timer
+onready var timerReturn = $TimerReturn
+onready var timerCooldown = $TimerCooldown
 onready var animationState = animationTree.get("parameters/playback")
 
+onready var audio = $KomturSFXPlayer
+
+onready var sound_1 = preload("res://resources/assets/sfx/komtur_attack1.mp3")
+onready var sound_2 = preload("res://resources/assets/sfx/komtur_attack2.mp3")
+onready var sound_3 = preload("res://resources/assets/sfx/komtur_attack3.mp3")
+onready var sound_4 = preload("res://resources/assets/sfx/komtur_attack4.mp3")
+
+var sounds : Array
+
+func _ready():
+	# randomize to make sure our random numbers are always random
+	randomize() 
+	# an array of all sounds
+	sounds = [ sound_1, sound_2, sound_3, sound_4 ] 
+	audio.autoplay = false
+	# play a random sound 
+
+enum State {IDLE, WALK, NEW_DIRECTION, RETURN, CHASE, ATTACK, COOLDOWN}
 var rng = RandomNumberGenerator.new()
 var current_state = State.IDLE
 var motion = Vector2(rng_direction(), rng_direction())
 var return_counter = 0
+var player = null
 
 func rng_direction():
 	return rng.randf() - .5
-enum State {IDLE, WALK, NEW_DIRECTION, RETURN}
 
 func _physics_process(_delta):
 	motion_speed = get_parent().get_node("Christine").motion_speed
@@ -35,8 +54,25 @@ func _physics_process(_delta):
 			walk(motion)
 			# area which is considered as home
 			if (start_position - position).length() < 2:
-				timer.start()
+				timerReturn.start()
 				current_state = State.IDLE
+		State.CHASE:
+			if player == null:
+				timerReturn.wait_time = 1
+				current_state = rng.randi_range(0,2)
+			else:
+				motion = position.direction_to(player.position)
+				walk(motion)
+		State.ATTACK:
+			_play_random_sound()
+			animationTree.set("parameters/Chop/blend_position", motion.normalized())
+			animationState.travel("Chop")
+			current_state = State.COOLDOWN
+			timerCooldown.wait_time = 30
+			timerCooldown.start()
+		State.COOLDOWN:
+			animationTree.set("parameters/Idle/blend_position", motion.normalized())
+			animationState.travel("Idle")
 				
 func walk(motion):
 	animationTree.set("parameters/Idle/blend_position", motion.normalized())
@@ -45,13 +81,49 @@ func walk(motion):
 	motion = motion.normalized() * motion_speed
 	move_and_slide(motion)
 
-func _on_Timer_timeout():
+func _on_TimerReturn_timeout():
 	return_counter += 1
 	if return_counter == max_return_counter:
-		timer.stop()
+		timerReturn.stop()
 		current_state = State.RETURN
 		return_counter = 0
 	else:
-		timer.wait_time = 1
-		# supposedly, there is a function randi_range(from,to), but it somehow doesn't exist 
-		current_state = randi() % (State.size()-1)
+		timerReturn.wait_time = 1
+		# supposedly, there is a function randi_range(from,to), but it somehow doesn't exist
+		# It's a function of the random number generator object :D 
+		current_state = rng.randi_range(0,2)
+
+
+func _on_KomturChaseArea_body_entered(body):
+	if player == null and body.name == "Christine":
+		player = body
+		current_state = State.CHASE
+		timerReturn.stop()
+
+func _on_KomturChaseArea_body_exited(body):
+	if player != null and body.name == "Christine":
+		player = null
+		timerReturn.wait_time = 1
+		current_state = rng.randi_range(0,2)
+
+func _on_KomturAttackArea_body_entered(body):
+	if body.name == "Christine":
+		current_state = State.ATTACK
+
+func _on_KomturAttackArea_body_exited(body):
+	if body.name == "Christine" and current_state != State.COOLDOWN:
+		current_state = State.CHASE
+	
+func _play_random_sound():
+	# get a random number between 0 and 3
+	var sound_index = randi() % 4 
+	# get a sound with random index
+	var sound = sounds[sound_index] 
+	# set the sound to the audio stream player
+	sound.loop = false
+	audio.stream = sound 
+	# play the sound
+	audio.play() 
+
+func _on_KomturSFXPlayer_finished():
+	audio.stop()
