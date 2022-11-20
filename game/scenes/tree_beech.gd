afcf7cec94c8d2a5f994816ffb43090f79e55c7b
d-schn <david.p.schneider@fu-berlin.de>
2022-11-20 14:47:57 +0100

    little bugfixes on chop
    chop now disables collision, beech counter updated, castle indicator checked

--- game/scenes/tree_beech.gd
+++ game/scenes/tree_beech.gd
@@ -4,19 +4,24 @@ onready var ischopped = false
 onready var chopfx = $AnimationLeafspell
 onready var beechsprite = $Oaktree
 onready var hole = $Hole
-onready var tween = $Tween
+onready var effect = $Spritetween
+onready var collision = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	effect.interpolate_property(beechsprite, 'scale', beechsprite.get_scale(), Vector2(0.1, 0.1), 0.2, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	#pass
 
 func chop():
 	ischopped = true
 	chopfx.visible = true
 	chopfx.play()
-	tween.interpolate_property(beechsprite, "transform/scale", null, Vector2(0,0), 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
-	tween.start()
-	beechsprite.visible = false
+	effect.start()
 	hole.visible = true
+	collision.disabled = true
 	#queue_free()
 
+
+
+func _on_Spritetween_tween_completed(object, key):
+	beechsprite.visible = false
