7b23d6a6a5118034acf109a196ac9cfee00d1a8b
g-oelk <g.oelkers@outlook.de>
2022-11-22 20:17:03 +0100

    Update DerGruene.gd

--- game/scenes/DerGruene.gd
+++ game/scenes/DerGruene.gd
@@ -13,11 +13,10 @@ onready var animationState = animationTree.get("parameters/playback")
 var rng = RandomNumberGenerator.new()
 var current_state = State.IDLE
 var motion = Vector2(rng_direction(), rng_direction())
-var return_counter = 0
 
 func rng_direction():
 	return rng.randf() - .5
-enum State {IDLE, WALK, NEW_DIRECTION, RETURN}
+enum State {IDLE, WALK, NEW_DIRECTION}
 
 func _physics_process(_delta):
 	match current_state:
@@ -28,14 +27,6 @@ func _physics_process(_delta):
 		State.NEW_DIRECTION:
 			motion = Vector2(rng_direction(), rng_direction())
 			current_state = State.WALK
-		State.RETURN:
-			var start_position = world.tilemap.map_to_world(world.start_position_komtur)
-			motion = start_position - position
-			walk(motion)
-			# area which is considered as home
-			if (start_position - position).length() < 2:
-				timer.start()
-				current_state = State.IDLE
 				
 func walk(motion):
 	animationTree.set("parameters/Idle/blend_position", motion.normalized())
@@ -45,12 +36,5 @@ func walk(motion):
 	move_and_slide(motion)
 
 func _on_Timer_timeout():
-	return_counter += 1
-	if return_counter == max_return_counter:
-		timer.stop()
-		current_state = State.RETURN
-		return_counter = 0
-	else:
-		timer.wait_time = 1
-		# supposedly, there is a function randi_range(from,to), but it somehow doesn't exist 
-		current_state = randi() % (State.size()-1)
+	timer.wait_time = 1
+	current_state = rng.randi_range(0,State.size()-1)
