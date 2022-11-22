7c7df037ea106c444ced14af5d7aff795693ee4e
Bernhard Irsigler <bernhard.irsigler@fu-berlin.de>
2022-11-22 20:41:01 +0100

    helper

--- game/scenes/christine.gd
+++ game/scenes/christine.gd
@@ -1,24 +1,28 @@
 extends KinematicBody2D
-
-const MOTION_SPEED = 160 # Pixels/second.
+#remove me
 const RUN_MULT = 10
+
+signal BeechChopped(inventory, count)
+signal BeechesExceeded
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var sprite = $Sprite
-onready var timer = $Timer
+onready var timer = $JumpTimer
 onready var dayTimer = $Timer2
 onready var area = $Area2D
 onready var beechCounterLabel = $BeechCounterLabel
 onready var daysLeftLabel = $DaysLeftLabel
-onready var tilemap = $"../TileMap_Ground"
 var running = false
 var jump_height = 40
 var jump_duration = 1
 var motion = Vector2()
 var direction = Vector2()
 var beech_count = 0
+var beech_inventory = 0
+var motion_speed = 300
+var animation_speed = 2
 
 enum State {IDLE, WALK, RUN, JUMP, CHOP}
 var current_state = State.IDLE
@@ -34,11 +38,9 @@ func _ready():
 	daysLeftLabel.add_font_override("normal_font", dynamic_font)
 
 func _physics_process(_delta):
-	
-	print(tilemap.map_to_world(position))
-	
-	daysLeftLabel.text = "noch %s Tage" % round(dayTimer.time_left / 60)
-	if beech_count < 10:
+	updateSpeed()
+	daysLeftLabel.text = "noch %s Tage" % round(dayTimer.time_left / 20)
+	if beech_count < 100:
 		beechCounterLabel.text = "Buchen: %s" % beech_count
 	else:
 		daysLeftLabel.text = ""
@@ -55,8 +57,8 @@ func _physics_process(_delta):
 	elif current_state != State.JUMP:
 		direction = motion.normalized()
 		current_state = State.WALK
-		
-	motion = direction * MOTION_SPEED
+
+	motion = direction * motion_speed
 	
 	if Input.is_action_just_pressed("run"):
 		running = not running
@@ -79,12 +81,12 @@ func _physics_process(_delta):
 			chop()
 			
 func idle():
-	animationTree.set("parameters/Idle/blend_position", direction)
+	animationTree.set("parameters/Idle/BlendSpace2D/blend_position", direction)
 	animationState.travel("Idle")
 	
 func walk(motion):
-	animationTree.set("parameters/Run/blend_position", direction)
-	animationState.travel("Run")
+	animationTree.set("parameters/Walk/BlendSpace2D/blend_position", direction)
+	animationState.travel("Walk")
 	move_and_slide(motion)
 
 func jump():
@@ -94,17 +96,47 @@ func jump():
 	move_and_slide(motion)
 	
 func chop():
-	animationTree.set("parameters/Chop/blend_position", direction)
+	animationTree.set("parameters/Chop/BlendSpace2D/blend_position", direction)
 	animationState.travel("Chop")
 
 	#This area is for collision layer/mask 2, the same as the one for beeches
 	for body in area.get_overlapping_bodies():
 		if body:
-			beech_count += 1
-		body.queue_free()
+			if beech_inventory > 4:
+				emit_signal("BeechesExceeded")
+			elif not body.ischopped:
+				beech_inventory += 1
+				emit_signal("BeechChopped", beech_inventory, beech_count)
+				body.chop()
+				#body.queue_free()
 		
 
 func _on_Timer_timeout():
 	timer.wait_time = jump_duration
 	animationState.travel("Run")
 	current_state = State.IDLE
+
+func _on_IntAreaCastle_body_entered(body):
+	if body.name == "Castle" and beech_inventory > 0:
+		beech_count += beech_inventory
+		beech_inventory = 0
+		emit_signal("BeechChopped", beech_inventory, beech_count)
+
+func _on_GUI_NewGame():
+	beech_count = 0
+	beech_inventory = 0
+	emit_signal("BeechChopped", beech_inventory, beech_count)
+
+func updateSpeed():
+	if Input.is_action_just_pressed("increase_animation_speed"):
+		animation_speed += .1
+	if Input.is_action_just_pressed("decrease_animation_speed"):
+		animation_speed -= .1
+	animationTree.set("parameters/Walk/TimeScale/scale",animation_speed)
+	animationTree.set("parameters/Idle/TimeScale/scale",animation_speed)
+	animationTree.set("parameters/Chop/TimeScale/scale",animation_speed)
+	
+	if Input.is_action_just_pressed("increase_motion_speed"):
+		motion_speed += 5
+	if Input.is_action_just_pressed("decrease_motion_speed"):
+		motion_speed -= 5
