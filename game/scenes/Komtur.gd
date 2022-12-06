0c787c06043385b4949cf5bb2e97ca37add3cebd
d-schn <david.p.schneider@fu-berlin.de>
2022-12-06 21:37:51 +0100

    Komtur interception
    Komtur will try to intercept Christine:
    - walk towards her if she is less than INTERCEPT_RADIUS from castle
    - walk towards the closest point on INTERCEPT_RADIUS from Christine
    Komtur will not chase Christine too far away from castle

--- game/scenes/Komtur.gd
+++ game/scenes/Komtur.gd
@@ -24,6 +24,16 @@ onready var sound_4 = preload("res://resources/assets/sfx/komtur_attack4.mp3")
 
 var sounds : Array
 
+enum State {IDLE, WALK, NEW_DIRECTION, INTERCEPT, RETURN, CHASE, ATTACK, COOLDOWN}
+var rng = RandomNumberGenerator.new()
+var current_state = State.IDLE
+var motion = Vector2(rng_direction(), rng_direction())
+var return_counter = 0
+var player = null
+var intercept_aim = Vector2(0,0)
+var CASTLE_POS = Vector2(0,0)
+var INTERCEPT_RADIUS = 1500
+
 func _ready():
 	randomize() 
 	sounds = [ sound_1, sound_2, sound_3, sound_4 ] 
@@ -31,16 +41,10 @@ func _ready():
 	animationTree.set("parameters/Walk/TimeScale/scale",animation_speed)
 	animationTree.set("parameters/Idle/TimeScale/scale",animation_speed)
 	animationTree.set("parameters/Chop/TimeScale/scale",animation_speed)
-
-enum State {IDLE, WALK, NEW_DIRECTION, RETURN, CHASE, ATTACK, COOLDOWN}
-var rng = RandomNumberGenerator.new()
-var current_state = State.IDLE
-var motion = Vector2(rng_direction(), rng_direction())
-var return_counter = 0
-var player = null
+	#CASTLE_POS = world.tilemap.map_to_world(world.start_position_castle)
 
 func _get_debug():
-	return "Pos: %s, St: %s" % [position.round(), State.keys()[current_state]]
+	return "Pos: %s, St: %s, Aim: %s" % [position.round(), State.keys()[current_state], intercept_aim.round()]
 
 func rng_direction():
 	return rng.randf() - .5
@@ -52,13 +56,29 @@ func _physics_process(_delta):
 		State.WALK:
 			walk(motion)
 		State.NEW_DIRECTION:
-			#Instead of any new direction, always walk towards 
-			#Christines position if inside a radius a around castle
+			motion = Vector2(rng_direction(), rng_direction())
+			current_state = State.WALK
+		State.INTERCEPT:
+			#walk towards Christines position if inside a radius a around castle
 			#The intersection of radius a and the line connecting christine and castle
 			#balance timer state change to 5s or more
 			#result: Christine staying still will result in Komtur finding her
-			motion = Vector2(rng_direction(), rng_direction())
-			current_state = State.WALK
+			timerStateChange.stop()
+			if intercept_aim.length() == 0:
+				var ChristineToCastle = christine.position - world.tilemap.map_to_world(world.start_position_castle)
+				if  ChristineToCastle.length() < INTERCEPT_RADIUS:
+					intercept_aim = christine.position
+					motion = position.direction_to(christine.position)
+				else:
+					intercept_aim = ChristineToCastle.normalized()*INTERCEPT_RADIUS + world.tilemap.map_to_world(world.start_position_castle)
+					motion = position.direction_to(intercept_aim)
+				#start timer so if Komtur gets stuck, he will do something else after some time
+				timerCooldown.start(10)
+			elif (position - intercept_aim).length() < 150:
+				timerRandomState()
+				intercept_aim = Vector2(0,0)
+			else:
+				walk(motion)
 		State.RETURN:
 			var start_position = world.tilemap.map_to_world(world.start_position_komtur)
 			motion = start_position - position
@@ -68,8 +88,13 @@ func _physics_process(_delta):
 				timerStateChange.start()
 				current_state = State.IDLE
 		State.CHASE:
+			#wrong state if play is null
 			if player == null:
 				timerRandomState()
+			#break off chase after certain distance from castle
+			elif (position - world.tilemap.map_to_world(world.start_position_castle)).length() > 1800:
+				timerRandomState()
+			#actual chase
 			else:
 				motion = position.direction_to(player.position)
 				walk(motion)
@@ -78,11 +103,11 @@ func _physics_process(_delta):
 		State.COOLDOWN:
 			pass
 				
-func walk(motion):
-	animationTree.set("parameters/Walk/BlendSpace2D/blend_position", motion.normalized())
+func walk(motionvec):
+	animationTree.set("parameters/Walk/BlendSpace2D/blend_position", motionvec.normalized())
 	animationState.travel("Walk")
-	motion = motion.normalized() * motion_speed
-	move_and_slide(motion)
+	motionvec = motionvec.normalized() * motion_speed
+	move_and_slide(motionvec)
 	
 func attack():
 	_play_random_sound()
@@ -94,25 +119,22 @@ func attack():
 	current_state = State.COOLDOWN
 	
 func timerRandomState():
-	current_state = rng.randi_range(0,2)
+	current_state = rng.randi_range(0,3)
 	timerStateChange.start(1)
-
-func _on_TimerReturn_timeout():
-	return_counter += 1
+	
+func _on_TimerStateChange_timeout():
 	if current_state == State.COOLDOWN:
 		pass
-	elif return_counter == max_return_counter:
-		timerStateChange.stop()
-		current_state = State.RETURN
-		return_counter = 0
 	else:
 		timerRandomState()
+	#pass # Replace with function body.
 
 func _on_KomturChaseArea_body_entered(body):
 	if player == null and body.name == "Christine":
 		player = body
 		current_state = State.CHASE
 		timerStateChange.stop()
+		intercept_aim = Vector2(0,0)
 
 func _on_KomturChaseArea_body_exited(body):
 	if player != null and body.name == "Christine":
@@ -149,3 +171,6 @@ func _on_TimerAttack_timeout():
 	animationState.travel("Idle")
 	timerCooldown.wait_time = 5
 	timerCooldown.start()
+
+
+
