d0cf585a546afe7731b98a9564319af79c314e35
Bernhard Irsigler <bernhard.irsigler@fu-berlin.de>
2023-02-26 18:41:17 +0100

    White beech outline shader
    outline flickers with frequency of 5 when selected and 20 when being chopped, respectively.
    
    The scene file of christine seemed to be misnamed.

--- game/scenes/tree_beech.gd
+++ game/scenes/tree_beech.gd
@@ -3,6 +3,11 @@ extends StaticBody2D
 
 var is_chopped = false
 var chopping_progress = 100
+var is_selected = false
+var is_being_chopped = false
+var outline_color = Color(1, 1, 1, 0)
+var time = 0
+var outline_frequency = 5
 
 onready var chop_fx = $AnimationLeafspell
 onready var beech_sprite = $Oaktree
@@ -15,7 +20,22 @@ func _ready():
 	effect.interpolate_property(beech_sprite, 'scale', beech_sprite.get_scale(), Vector2(0.1, 0.1), 0.2, Tween.TRANS_QUAD, Tween.EASE_OUT)
 
 
+func _physics_process(delta):
+	if is_selected:
+		time += delta
+		if is_being_chopped:
+			outline_frequency = 20
+		else:
+			outline_frequency = 5
+		outline_color = Color(1, 1, 1, .5 + .5 * pow(sin(outline_frequency * time), 2))
+	else:
+		time = 0
+		outline_color = Color(1, 1, 1, 0)
+	beech_sprite.get_material().set_shader_param("outline_color", outline_color)
+
+
 func chop():
+	is_being_chopped = true
 	chopping_progress -= 1
 	if chopping_progress <= 0:
 		is_chopped = true
